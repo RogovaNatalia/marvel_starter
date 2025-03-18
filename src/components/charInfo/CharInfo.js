8e7f34e854ec89aa00e4601289d1f8a7c1bc5485
Your Name <your_email@whatever.com>
2025-03-18 14:46:19 +0100

    ref

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
+
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
