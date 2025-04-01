092baa65004c4342387bf6a232cba4d6f61471f8
Your Name <kamelia-17@mail.ru>
2025-04-01 11:02:11 +0200

    практика собственных хуков на проекте

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,17 +1,14 @@
 import { useState, useEffect } from "react";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 
 const RandomChar = () => {
   const [char, setChar] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const marvelService = new MarvelService();
+  const { loading, error, getCharacter, clearError } = useMarvelService();
 
   useEffect(() => {
     updateChar();
@@ -23,28 +20,18 @@ const RandomChar = () => {
   }, []);
 
   const onCharLoaded = (char) => {
-    setLoading(false);
     setChar(char);
   };
 
-  const onCharLoading = () => {
-    setLoading(true);
-  };
-
-  const onError = () => {
-    setLoading(false);
-    setError(true);
-  };
-
   const updateChar = () => {
-    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    onCharLoading();
-    marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+    clearError();
+    const id = Math.floor(Math.random() * 9 + 1);
+    getCharacter(id).then(onCharLoaded);
   };
 
   const errorMessage = error ? <ErrorMessage /> : null;
   const spinner = loading ? <Spinner /> : null;
-  const content = !(loading || error) ? <View char={char} /> : null;
+  const content = !(loading || error || !char) ? <View char={char} /> : null;
 
   return (
     <div className="randomchar">
@@ -90,7 +77,7 @@ const View = ({ char }) => {
         <p className="randomchar__descr">{description}</p>
         <div className="randomchar__btns">
           <a href={homepage} className="button button__main">
-            <div className="inner">homepage test</div>
+            <div className="inner">homepage</div>
           </a>
           <a href={wiki} className="button button__secondary">
             <div className="inner">Wiki</div>
