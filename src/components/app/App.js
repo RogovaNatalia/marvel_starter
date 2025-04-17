edfc74d2d507592c697fa950570652865b2e8dee
Your Name <kamelia-17@mail.ru>
2025-04-17 12:38:05 +0200

    Formik, Yup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router";
 import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from "../spinner/Spinner";
@@ -7,7 +7,13 @@ import Spinner from "../spinner/Spinner";
 const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsPage = lazy(() => import("../pages/ComicsPage"));
-const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+const SingleComicLayout = lazy(() =>
+  import("../pages/singleComicLayout/SingleComicLayout")
+);
+const SingleCharacterLayout = lazy(() =>
+  import("../pages/singleCharacterLayout/SingleCharacterLayout")
+);
+const SinglePage = lazy(() => import("../pages/SinglePage"));
 
 const App = () => {
   return (
@@ -15,14 +21,24 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Suspense fallback={Spinner}>
+          <Suspense fallback={<Spinner />}>
             <Routes>
               <Route path="/" element={<MainPage />} />
-              <Route exact path="/comics" element={<ComicsPage />} />
+              <Route path="/comics" element={<ComicsPage />} />
               <Route
-                exact
-                path="/comics/:comicId"
-                element={<SingleComicPage />}
+                path="/comics/:id"
+                element={
+                  <SinglePage Component={SingleComicLayout} dataType="comic" />
+                }
+              />
+              <Route
+                path="/characters/:id"
+                element={
+                  <SinglePage
+                    Component={SingleCharacterLayout}
+                    dataType="character"
+                  />
+                }
               />
               <Route path="*" element={<Page404 />} />
             </Routes>
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,9 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
+import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import useMarvelService from "../../services/MarvelService";
+
 import "./charList.scss";
 
 const CharList = (props) => {
@@ -23,16 +25,15 @@ const CharList = (props) => {
     getAllCharacters(offset).then(onCharListLoaded);
   };
 
-  const onCharListLoaded = (newCharList) => {
+  const onCharListLoaded = async (newCharList) => {
     let ended = false;
     if (newCharList.length < 9) {
       ended = true;
     }
-
-    setCharList((charList) => [...charList, ...newCharList]);
-    setNewItemLoading((newItemLoading) => false);
-    setOffset((offset) => offset + 9);
-    setCharEnded((charEnded) => ended);
+    setCharList([...charList, ...newCharList]);
+    setNewItemLoading(false);
+    setOffset(offset + 9);
+    setCharEnded(ended);
   };
 
   const itemRefs = useRef([]);
@@ -56,29 +57,34 @@ const CharList = (props) => {
       }
 
       return (
-        <li
-          className="char__item"
-          tabIndex={0}
-          ref={(el) => (itemRefs.current[i] = el)}
-          key={item.id}
-          onClick={() => {
-            props.onCharSelected(item.id);
-            focusOnItem(i);
-          }}
-          onKeyPress={(e) => {
-            if (e.key === " " || e.key === "Enter") {
+        <CSSTransition key={item.id} timeout={500} classNames="char__item">
+          <li
+            className="char__item"
+            tabIndex={0}
+            ref={(el) => (itemRefs.current[i] = el)}
+            onClick={() => {
               props.onCharSelected(item.id);
               focusOnItem(i);
-            }
-          }}
-        >
-          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
-          <div className="char__name">{item.name}</div>
-        </li>
+            }}
+            onKeyPress={(e) => {
+              if (e.key === " " || e.key === "Enter") {
+                props.onCharSelected(item.id);
+                focusOnItem(i);
+              }
+            }}
+          >
+            <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+            <div className="char__name">{item.name}</div>
+          </li>
+        </CSSTransition>
       );
     });
 
-    return <ul className="char__grid">{items}</ul>;
+    return (
+      <ul className="char__grid">
+        <TransitionGroup component={null}>{items}</TransitionGroup>
+      </ul>
+    );
   }
 
   const items = renderItems(charList);
@@ -92,9 +98,9 @@ const CharList = (props) => {
       {spinner}
       {items}
       <button
-        className="button button__main button__long"
         disabled={newItemLoading}
         style={{ display: charEnded ? "none" : "block" }}
+        className="button button__main button__long"
         onClick={() => onRequest(offset)}
       >
         <div className="inner">load more</div>
